55b9b1de0a89a000887909f06f58e3f716045de9
Rodolfo Bandeira <rbandeira@demacmedia.com>
2016-03-30 15:38:57 -0400

    dynamodb-session-handler

---
+++ dynamodb-session-handler/DynamoDB-Session-Handler-Example.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\DynamoDb\SessionHandler;
+
+$sessionHandler = SessionHandler::fromClient($dynamoDb, [
+        'table_name' => 'sessions'
+        ]);
+
+$sessionHandler->register();
+
+
+// Start the session
+session_start();
+
+// Alter the session data
+$_SESSION['user.name'] = 'Rodolfo Bandeira';
+$_SESSION['user.role'] = 'it';
+
+// Close the session (optional, but recommended)
+session_write_close();
+
